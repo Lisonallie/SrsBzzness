854190b06ed1e50a2b1d2c8d2c4adf5e7fb488e5
Lisonallie <allisonvanlinden@gmail.com>
2019-06-06 11:36:41 +0200

    Fix problem in day programming

--- assets/myscript.js
+++ assets/myscript.js
@@ -4,12 +4,12 @@ var clock = document.getElementById("clock");
 
 
 function starter() {
-    if (today.getHours() >= 10 && today.getHours() < 18 && today.getDay() < 4) {
+    if (today.getHours() >= 10 && today.getHours() < 18 && today.getDay() < 5) {
         write.innerHTML = "Open!";
     } else {
         write.innerHTML = "Closed.";
     }
-    if (today.getDay() = 4 && today.getHours() < 15) {
+    if (today.getDay() = 5 && today.getHours() < 15) {
         write.innerHTML = "Open!";
     }
 }
